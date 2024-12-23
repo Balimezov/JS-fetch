2f4eeec22ba85afd4bff4f767ce41d56875bf4f6
Nikola <nikbalimezov@gmail.com>
2024-12-23 22:27:45 +0200

    Add files via upload

---
+++ util.js
@@ -0,0 +1,17 @@
+//Util function
+function createElement(tag, properties, container) {
+    const element = document.createElement(tag);
+
+    Object.keys(properties).forEach(key => {
+        if ( typeof properties[key] === 'object' ) {
+            element[key] ??= {};
+            Object.assign(element[key], properties[key]);
+        } else {
+            element[key] = properties[key];
+        }
+    });
+    
+    if ( container ) container.append(element);
+    
+    return element;
+}
